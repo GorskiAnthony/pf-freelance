688c5318380ab578715b80fab19132afccd82346
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-03-29 15:14:32 +0100

    ✨ (ScrollTop.tsx): add ScrollToTop component to automatically scroll to the top of the page when the route changes 📝 (Layout.tsx): import and include ScrollToTop component in the Layout to ensure the page scrolls to the top on route changes

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../helpers/ScrollTop";
 function Layout() {
 	return (
 		<div className="min-h-screen bg-zinc-50">
+			<ScrollToTop />
 			<Navbar />
 			<Outlet />
 			<Footer />
